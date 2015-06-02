acc957c4a04d1b9a7a0475115fab77b556365d02
Alexander Shniperson <alex.shniperson@gmail.com>
2015-06-02 10:27:44 +0400

    fix path to pwd file

--- glassfish-start.sh
+++ glassfish-start.sh
@@ -8,18 +8,13 @@ set -e
 hostIP="$(hostname -i)"
 export hostIP
 export LANG=ru_RU.UTF-8
-export glassfish_pwdfile="AS_ADMIN_PASSWORD=glassfish"
 mkdir -p /opt/glassfish/applications
 chown -R glassfish:glassfish /opt/glassfish
-whoami
 su glassfish <<EOSU
-whoami
-pwd
-ls ../glassfish/domains/
 echo "hostIP=${hostIP}"
 ./asadmin start-domain domain1
-find /opt/glassfish/applications/ -name "*-ds.xml" -exec echo "Installing ds resources from " {} \; -exec ./asadmin --user admin --passwordfile ${glassfish_pwdfile} --echo=true --interactive=false add-resources  {} \; -exec echo "Done installing " {} \;
-find /opt/glassfish/applications/ -name "*.war" -exec echo "Installing application " {} \; -exec ./asadmin --user admin --passwordfile ${glassfish_pwdfile} deploy {} \; -exec echo "Done deploying " {} \;
+find /opt/glassfish/applications/ -name "*-ds.xml" -exec echo "Installing ds resources from " {} \; -exec ./asadmin --user admin --passwordfile ./pwdfile --echo=true --interactive=false add-resources  {} \; -exec echo "Done installing " {} \;
+find /opt/glassfish/applications/ -name "*.war" -exec echo "Installing application " {} \; -exec ./asadmin --user admin --passwordfile ./pwdfile deploy {} \; -exec echo "Done deploying " {} \;
 EOSU
 echo "Startup done"
 
